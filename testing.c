639603e3162a9151579d19d8394123681a3b5e95
cr <me@example.com>
2020-04-11 13:57:52 +0200

    added timing against sprintf

--- testing.c
+++ testing.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 #include "itoa.h"
 
@@ -40,11 +41,70 @@ int itoa_recursive_04() {
     return 0;
 }
 
+int itoa_recursive_time01() {
+    n = 0xffffffff;
+    clock_t begin = clock();
+    /* here, do your time-consuming job */
+    //memset(buf, 0xaa, 21);
+    for (uint32_t i = 0; i < 1000*1000; i++)
+        itoa_recursive(n, buf);
+
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("%s: %f\n", __func__);
+    return 0;
+}
+int itoa_recursive_time02() {
+    n = 0x1;
+    clock_t begin = clock();
+    /* here, do your time-consuming job */
+    //memset(buf, 0xaa, 21);
+    for (uint32_t i = 0; i < 1000*1000; i++)
+        itoa_recursive(n, buf);
+
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("%s: %f\n", __func__);
+    return 0;
+}
+
+int itoa_sprintf_time01() {
+    n = 0xffffffff;
+    clock_t begin = clock();
+    /* here, do your time-consuming job */
+    //memset(buf, 0xaa, 21);
+    for (uint32_t i = 0; i < 1000*1000; i++)
+        sprintf(buf, "%d", n);
+
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("%s: %f\n", __func__);
+    return 0;
+}
+
+int itoa_sprintf_time02() {
+    n = 0x1;
+    clock_t begin = clock();
+    /* here, do your time-consuming job */
+    //memset(buf, 0xaa, 21);
+    for (uint32_t i = 0; i < 1000*1000; i++)
+        sprintf(buf, "%d", n);
+
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("%s: %f\n", __func__);
+    return 0;
+}
+
 int all_tests() {
     _verify(itoa_recursive_01);
     _verify(itoa_recursive_02);
     _verify(itoa_recursive_03);
     _verify(itoa_recursive_04);
+    _verify(itoa_recursive_time01);
+    _verify(itoa_sprintf_time01);
+    _verify(itoa_recursive_time02);
+    _verify(itoa_sprintf_time02);
     return 0;
 }
 
@@ -52,6 +112,9 @@ int main(int argc, char **argv) {
     int result = all_tests();
     if (result == 0)
         printf("PASSED\n");
+    else
+        printf("FAIL\n");
+
     printf("Tests run: %d\n", tests_run);
 
     return result != 0;
